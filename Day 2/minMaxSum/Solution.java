ec618dca57bb6ba4d5eb5e6d2d7618bc422461c6
princi-pk <75043174+princi-pk@users.noreply.github.com>
2021-02-14 10:53:20 +0530

    Add files via upload

---
+++ Day 2/minMaxSum/Solution.java
@@ -0,0 +1,51 @@
+package minMaxSum;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        int temp = 0;
+        for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < 4; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+        long sum=0;
+        long sum2=0;
+        for (int i = 0; i < 4; i++) {
+            sum=sum+arr[i];
+            sum2=sum2+arr[i+1];
+        }
+        System.out.println(sum+" "+sum2);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
